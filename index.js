a5849dd042287f255f1f2bec6a29f86ac65b1a89
aksaxena <saxenaalokkumar878@gmail.com>
2025-03-30 01:53:01 +0530

    match delimters in numbers

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ function add(numbers) {
     if (!numbers) return 0;
     //define possible delimiter
     let delimiter = /,|\n/;
+    //match delimiters in numbers
+    let customDelimiterMatch = numbers.match(/^\/\/(.+)\n/);
 }
